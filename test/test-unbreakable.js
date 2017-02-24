2ff1647dd4625f9271acec050a1e099f395f5aea
Yoshiaki Kawazu <kawaz@kawaz-mbp.local>
2017-02-24 15:47:33 +0900

    Add test for combining character sequence

--- test/test-unbreakable.js
+++ test/test-unbreakable.js
@@ -11,3 +11,18 @@ test('If includes space, replace to nbsp', t => {
   t.not('ヽ(=´▽ `=)ﾉ', unbreakable('ヽ(=´▽ `=)ﾉ'))
   t.is('ヽ\u2060(\u2060=\u2060´\u2060▽\u2060\u00a0\u2060`\u2060=\u2060)\u2060ﾉ', unbreakable('ヽ(=´▽ `=)ﾉ'))
 })
+
+test('If includes combining character sqeuence, not insert WJ in it', t => {
+  const kaomoji = '(ఠ్ఠ ˓̭ ఠ్ఠ)'
+  const expect = [
+    '(',
+    'ఠ్ఠ',
+    '',
+    '\u00a0',
+    '˓̭',
+    '\u00a0',
+    'ఠ్ఠ',
+    ')',
+  ].join('\u2060')
+  t.is(expect, unbreakable(kaomoji))
+})
